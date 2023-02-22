dea4ad076868a5e0d225ef88cd76c4817262c8fd
Zachary Ebert <zne0001@mix.wvu.edu>
2023-02-22 15:57:27 -0500

    Testing Laptop andd Fixing Home

---
+++ my-app/src/data/homeUnit.tsx
@@ -0,0 +1,9 @@
+import { HomeUnitProps } from "../models/homeUnitProps"
+
+export const HomePageUnit: HomeUnitProps[] = [{
+    image: "https://cdn.shopify.com/s/files/1/0311/6433/4213/files/83708ed3a1e20b5cd2f09369d690d4c0_480x480.png?v=1643930157",
+    title: "Sword and Shield: Base Set",
+    desc1: "This set is the first to welcome you to the ",
+    desc2: "Galar Region! Zacain and Zamazenta are first",
+    desc3: "Introduced, along with V and Vmax Sw$Sh Cards!",
+},]
---
+++ my-app/src/models/homeUnitProps.tsx
@@ -0,0 +1,8 @@
+export interface HomeUnitProps{
+    image: string;
+    title: string;
+    desc1: string;
+    desc2?: string;
+    desc3?: string;
+    ranking?: number;
+}
